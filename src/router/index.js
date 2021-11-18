0feef3d00d88fddc2a0da278801920b5feeebccd
devdks <david.sovitrat@gmail.com>
2021-11-18 18:08:58 +0100

    Refactoring Details Page (Basic Routing)
    - Split EventDetails into 3 parts:
        - Details
        - Register
        - Edit
    - Modify the routes to match each part
    Issues:
     - Too much duplicated code

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
-import EventDetails from "../views/EventDetails.vue"
+import EventDetails from "../views/event/Details.vue"
+import EventRegister from "../views/event/Register.vue"
+import EventEdit from "../views/event/Edit.vue"
 import EventList from "../views/EventList.vue"
 
 const routes = [
@@ -14,6 +16,18 @@ const routes = [
     name: "EventDetails",
     props: true,
     component: EventDetails,
+  },
+  {
+    path: "/event/:id/register",
+    name: "EventRegister",
+    props: true,
+    component: EventRegister,
+  },
+  {
+    path: "/event/:id/edit",
+    name: "EventEdit",
+    props: true,
+    component: EventEdit,
   },
   {
     path: "/about",
