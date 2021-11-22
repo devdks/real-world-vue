1f1b1897824733d668262fa496c016290f23ad3c
devdks <david.sovitrat@gmail.com>
2021-11-22 18:20:08 +0100

    Programmatic Navigation added

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="register-panel" v-if="event">
     <p>Register for the event here</p>
+    <button @click="register">Register Me!</button>
   </div>
 </template>
 
 <script>
 export default {
   props: ["event"],
+  methods: {
+    register() {
+      // Call to API
+      // If registered then redirect to event details
+      this.$router.push({
+        name: "EventDetails",
+        // params: { id: this.event.id }, <-- no need because we're inside the register page
+      })
+    },
+  },
 }
 </script>
 
